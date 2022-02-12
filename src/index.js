b0ef8315ad1ffe90c44d1f493e9adf594983ad4d
Nataliia Nazarko <90257127+Nataliia-Nazarko@users.noreply.github.com>
2022-02-12 20:15:04 +0200

    main-file

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './css/styles.css';
+import './js/markup';
+import './js/service-api';
+
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
