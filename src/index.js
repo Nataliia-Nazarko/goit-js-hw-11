86c6889bf862c386a02ee51c1945404eb447edbf
Nataliia Nazarko <90257127+Nataliia-Nazarko@users.noreply.github.com>
2022-02-15 21:51:46 +0200

    loadMoreBtn

--- src/index.js
+++ src/index.js
@@ -7,40 +7,16 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const refs = {
-    inputForm: document.querySelector('input'),
+    form: document.querySelector('.search-form'),
     submitBtn: document.querySelector('button[type=submit]'),
     gallery: document.querySelector('.gallery'),
+    loadMoreBtn: document.querySelector('.load-more')
 }
 
-refs.inputForm.addEventListener('submit', onSubmitBtn);
+refs.loadMoreBtn.classList.add('is-hidden');
+refs.form.addEventListener('submit', onSubmitBtn);
 
 const loadbildsApi = new FetchBildsAPI();
-
-
-function onSubmitBtn(e) {
-  e.preventDefault();
-
-  const isFilled = e.currentTarget.elements.searchQuery.value;
-  
-  if (isFilled) {
-    loadService.searchQuery = isFilled;
-    loadService.resetPage();
-    refs.gallery.innerHTML = '';
-    
-    loadbildsApi.getBilds()
-      .then(renderGallery)
-      .catch(error => {
-        return Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-      })
-  }
-
-  function renderGallery(data) {
-    const markupData = markup(data);
-    return refs.gallery.insertAdjacentHTML('beforeend', markupData);
-  }
-}
-
-console.log(onSubmitBtn())
 
 let modalGallery = new SimpleLightbox('.gallery a', {
     caption: true,
@@ -49,4 +25,54 @@ let modalGallery = new SimpleLightbox('.gallery a', {
     captionDelay: 250,    
 }); 
 
+function onSubmitBtn(e) {
+  e.preventDefault();
+
+  const inputText = e.currentTarget.elements.searchQuery.value;
+  
+  if (inputText) {
+    loadbildsApi.searchQuery = inputText;
+    loadbildsApi.resetPage();
+    refs.gallery.innerHTML = '';
+    
+    
+    loadbildsApi.getBilds()
+      .then(renderGallery)
+      .catch(error => {
+    console.log(error);
+  });
+  }
+}
+  
+function renderGallery(data) {
+   
+  if (data.data.totalHits === 0) {
+    refs.loadMoreBtn.classList.add('is-hidden');
+    return Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    ;
+  }
+  refs.loadMoreBtn.classList.remove('is-hidden');
+   if (data.data.totalHits !== 0 && data.data.hits.length === 0) {
+     Notiflix.Notify.warning(`We're sorry, but you've reached the end of search results.`);
+    ;
+   }
+  
+  
+  refs.gallery.insertAdjacentHTML('afterbegin', markup(data.data.hits));
+
+  modalGallery.refresh();
+
+  if (loadbildsApi.pageNumber === 2) {
+    Notiflix.Notify.info(`Hooray! We found ${data.data.totalHits} images.`);
+  } else {
+    const { height: cardHeight } = refs.gallery.firstElementChild.getBoundingClientRect();
+    window.scrollBy({
+      top: cardHeight * 2 + 120,
+      behavior: 'smooth',
+    });
+  }
+}
+
+
+/* refs.loadMoreBtn.addEventListener('click', onSubmitBtn); */
 
